ae5abd263422669e3bc15c03633151ffd86f3c7d
Conor Sexton <sexton.conor@gmail.com>
2017-02-28 13:24:52 +0000

    Changed to cpp

---
+++ Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lARgsub_lite -lARvideo -lAR -lARICP -lAR -lglut -lGLU -lGL -lX11 -lm -lpthread -ljpeg
+
---
+++ Debug/src/PianoHero.d
@@ -0,0 +1,40 @@
+src/PianoHero.o: ../src/PianoHero.cpp \
+ /home/conor/SDKs/ARToolKit5/include/AR/config.h \
+ /home/conor/SDKs/ARToolKit5/include/AR/video.h \
+ /home/conor/SDKs/ARToolKit5/include/AR/ar.h \
+ /home/conor/SDKs/ARToolKit5/include/AR/arConfig.h \
+ /home/conor/SDKs/ARToolKit5/include/AR/matrix.h \
+ /home/conor/SDKs/ARToolKit5/include/AR/icp.h \
+ /home/conor/SDKs/ARToolKit5/include/AR/icpCore.h \
+ /home/conor/SDKs/ARToolKit5/include/AR/param.h \
+ /home/conor/SDKs/ARToolKit5/include/AR/arImageProc.h \
+ /home/conor/SDKs/ARToolKit5/include/AR/videoConfig.h \
+ /home/conor/SDKs/ARToolKit5/include/AR/sys/videoDummy.h \
+ /home/conor/SDKs/ARToolKit5/include/AR/sys/videoLinuxV4L2.h \
+ /home/conor/SDKs/ARToolKit5/include/AR/gsub_lite.h
+
+/home/conor/SDKs/ARToolKit5/include/AR/config.h:
+
+/home/conor/SDKs/ARToolKit5/include/AR/video.h:
+
+/home/conor/SDKs/ARToolKit5/include/AR/ar.h:
+
+/home/conor/SDKs/ARToolKit5/include/AR/arConfig.h:
+
+/home/conor/SDKs/ARToolKit5/include/AR/matrix.h:
+
+/home/conor/SDKs/ARToolKit5/include/AR/icp.h:
+
+/home/conor/SDKs/ARToolKit5/include/AR/icpCore.h:
+
+/home/conor/SDKs/ARToolKit5/include/AR/param.h:
+
+/home/conor/SDKs/ARToolKit5/include/AR/arImageProc.h:
+
+/home/conor/SDKs/ARToolKit5/include/AR/videoConfig.h:
+
+/home/conor/SDKs/ARToolKit5/include/AR/sys/videoDummy.h:
+
+/home/conor/SDKs/ARToolKit5/include/AR/sys/videoLinuxV4L2.h:
+
+/home/conor/SDKs/ARToolKit5/include/AR/gsub_lite.h:
